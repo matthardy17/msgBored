df733f57b3b9b945fa66d92e9217982ee03ecc40
matt hardy <matthewhardy17@gmail.com>
2021-01-10 21:12:30 +0000

    created a service for the get posts method within the posts controller

---
+++ app/services/posts_for_branch_service.rb
@@ -0,0 +1,20 @@
+class PostsForBranchService
+  def initialize(params)
+    @search = params[:search]
+    @category = params[:category]
+    @branch = params[:branch]
+  end
+
+  # get posts depending on the request
+  def call
+    if @category.blank? && @search.blank?
+      Post.by_branch(@branch).all
+    elsif @category.blank? && @search.present?
+      Post.by_branch(@branch).search(@search)
+    elsif @category.present? && @search.blank?
+      Post.by_category(@branch, @category)
+    elsif @category.present? && @search.present?
+      Post.by_category(@branch, @category).search(@search)
+    end
+  end
+end
